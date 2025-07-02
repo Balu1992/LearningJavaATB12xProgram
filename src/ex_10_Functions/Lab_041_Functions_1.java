247eac55850d0c8599e1ac1b8ed765ff32f1d355
Balaji <balu.bs567@gmail.com>
2025-07-02 22:39:49 +0530

    Functions-1

---
+++ src/ex_10_Functions/Lab_041_Functions_1.java
@@ -0,0 +1,14 @@
+package ex_10_Functions;
+
+public class Lab_041_Functions_1 {
+    public static void main(String[] args) {
+        String r=output();
+    }
+    static void nooutput(){
+        System.out.println("I wont print anything");
+    }
+    static String output(){
+        System.out.println("I will print");
+        return "Balu";
+    }
+}
---
+++ src/ex_10_Functions/Lab_042_Functions_Sumof2nos.java
@@ -0,0 +1,11 @@
+package ex_10_Functions;
+
+public class Lab_042_Functions_Sumof2nos {
+    public static void main(String[] args) {
+        int r1=sum_of_2nos(44,55);
+        System.out.println("Sum is:"+r1);
+    }
+    static int sum_of_2nos(int first,int second){
+        return first+second;
+    }
+}
